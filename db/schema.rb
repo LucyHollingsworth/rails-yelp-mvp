f01e0dd68e337acef8df99a5d9dde925f866c9cc
Lucy Hollingsworth <lucyholl@me.com>
2024-04-24 13:16:11 +0100

    add review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_24_121213) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_121511) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "category"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
